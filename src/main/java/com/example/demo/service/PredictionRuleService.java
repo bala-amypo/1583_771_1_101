75bad9a308b46e98f12a7da5916eac91ca06cc11
bala-amypo <bala@amypo.in>
2025-12-23 09:38:07 +0000

    User commit

--- src/main/java/com/example/demo/service/PredictionRuleService.java
+++ src/main/java/com/example/demo/service/PredictionRuleService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PredictionRuleService {
 
-    LocalDate predictRestockDate(Long stockRecordId);
+    LocalDate predictionRestockDate(Long stockRecordId);
 
     List<PredictionRule> getAllRules();
 
