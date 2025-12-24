b2bb3efe694870b983499293c827d0c98bdf6b64
bala-amypo <bala@amypo.in>
2025-12-24 10:09:14 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -7,8 +7,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@Getter
-@Setter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -18,20 +17,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String productName;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String sku;
 
-    private int quantity;
-
-    private double price;
+    private String category;
 
     private LocalDateTime createdAt;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
 }
