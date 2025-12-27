6445b2363aac1caf846fd45d1921f738b6e54924
bala-amypo <bala@amypo.in>
2025-12-27 03:45:26 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
-
-
 package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
@@ -13,33 +11,30 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-
-        SecurityScheme jwtScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
         return new OpenAPI()
-               
+
+                // API Info
+                .info(new Info()
+                        .title("JWT Demo API")
+                        .version("1.0")
+                        .description("Simple JWT Demo Project for Students"))
+
+                // Server Configuration
                 .servers(List.of(
                         new Server().url("https://9107.pro604cr.amypo.ai")
                 ))
 
-              
-                .addSecurityItem(
-                        new SecurityRequirement().addList("BearerAuth")
-                )
-
-                
-                .components(
-                        new Components().addSecuritySchemes(
-                                "BearerAuth", jwtScheme
-                        )
-                );
+                // Security Configuration (JWT Bearer)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
     }
 }
