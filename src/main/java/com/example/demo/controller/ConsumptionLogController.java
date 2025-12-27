2704783ac04a94887c645992780f2d5d97c43b47
bala-amypo <bala@amypo.in>
2025-12-27 03:52:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/ConsumptionLogController.java
+++ src/main/java/com/example/demo/controller/ConsumptionLogController.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/consumption")
-@SecurityRequire
 public class ConsumptionLogController {
 
     private final ConsumptionLogService consumptionLogService;
